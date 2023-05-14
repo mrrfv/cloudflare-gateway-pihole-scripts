bd8422597db43b9333d0afffb7b7782ffa699f50
Norbert <joninorbert@icloud.com>
2023-05-14 22:36:44 +0200

    list

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -5,30 +5,29 @@ touch whitelist.csv
 
 # declare an array of urls
 urls=(
-    https://raw.githubusercontent.com/norbertjoni/cloudflare-gateway-pihole-scripts/main/hup.txt
-    # https://raw.githubusercontent.com/im-sm/Pi-hole-Torrent-Blocklist/main/all-torrent-trackres.txt
-    # https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
-    # https://raw.githubusercontent.com/hagezi/dns-blocklists/main/whitelist.txt
-    # https://raw.githubusercontent.com/TogoFire-Home/AD-Settings/main/Filters/whitelist.txt
-    # https://raw.githubusercontent.com/freekers/whitelist/master/domains/whitelist.txt
-    # https://raw.githubusercontent.com/DandelionSprout/AdGuard-Home-Whitelist/master/whitelist.txt
-    # https://www.aadvantageeshopping.com/adBlockWhitelist.php
-    # https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt
-    # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt
-    # https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/issues.txt
-    # https://raw.githubusercontent.com/hagezi/dns-blocklists/main/whitelist-referral.txt
-    # https://raw.githubusercontent.com/mawenjian/china-cdn-domain-whitelist/master/china-cdn-domain-whitelist.txt
-    # https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/hostnames.whitelist.txt
-    # https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/mac.txt
-    # https://raw.githubusercontent.com/boutetnico/url-shorteners/master/list.txt
-    # https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/windows.txt
-    # https://raw.githubusercontent.com/Dogino/Discord-Phishing-URLs/main/official-domains.txt
-    # https://raw.githubusercontent.com/ookangzheng/blahdns/master/hosts/whitelist.txt
-    # https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/android.txt
-    # https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt
-    # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt
-    # https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
-    # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
+    https://raw.githubusercontent.com/im-sm/Pi-hole-Torrent-Blocklist/main/all-torrent-trackres.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
+    https://raw.githubusercontent.com/hagezi/dns-blocklists/main/whitelist.txt
+    https://raw.githubusercontent.com/TogoFire-Home/AD-Settings/main/Filters/whitelist.txt
+    https://raw.githubusercontent.com/freekers/whitelist/master/domains/whitelist.txt
+    https://raw.githubusercontent.com/DandelionSprout/AdGuard-Home-Whitelist/master/whitelist.txt
+    https://www.aadvantageeshopping.com/adBlockWhitelist.php
+    https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt
+    https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/issues.txt
+    https://raw.githubusercontent.com/hagezi/dns-blocklists/main/whitelist-referral.txt
+    https://raw.githubusercontent.com/mawenjian/china-cdn-domain-whitelist/master/china-cdn-domain-whitelist.txt
+    https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/hostnames.whitelist.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/mac.txt
+    https://raw.githubusercontent.com/boutetnico/url-shorteners/master/list.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/windows.txt
+    https://raw.githubusercontent.com/Dogino/Discord-Phishing-URLs/main/official-domains.txt
+    https://raw.githubusercontent.com/ookangzheng/blahdns/master/hosts/whitelist.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/android.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt
+    https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
+    https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
 
 )
 
