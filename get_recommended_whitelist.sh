c8111b038029d713fa6535340e12d8bfe17ed868
Norbert <joninorbert@icloud.com>
2023-06-30 09:28:11 +0200

    test is done

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -7,7 +7,6 @@ touch whitelist.csv
 
 # declare an array of urls
 urls=(
-      https://adaway.org/hosts.txt #test
     https://raw.githubusercontent.com/im-sm/Pi-hole-Torrent-Blocklist/main/all-torrent-trackres.txt
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
     https://raw.githubusercontent.com/hagezi/dns-blocklists/main/whitelist.txt
