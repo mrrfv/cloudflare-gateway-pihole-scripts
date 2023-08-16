ba5dfc83164ba871448ebb8e7cdceb6a5b7d08db
dns <115779583+tp-dm@users.noreply.github.com>
2023-08-16 19:26:31 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -6,23 +6,15 @@ touch input.csv
 # declare an array of urls
 urls=(
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts
   https://raw.githubusercontent.com/olbat/ut1-blacklists/master/blacklists/gambling/domains
   https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts
-  https://raw.githubusercontent.com/sjhgvr/oisd/main/domainswild_nsfw.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/dyndns.txt
-  https://raw.githubusercontent.com/elliotwutingfeng/Inversion-DNSBL-Blocklists/main/Google_hostnames.txt
   https://raw.githubusercontent.com/mitchellkrogza/The-Big-List-of-Hacked-Malware-Web-Sites/master/hosts
   https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareDomains.txt
-  https://raw.githubusercontent.com/stamparm/aux/master/maltrail-malware-domains.txt
-  https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardMobileSpyware.txt
   https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt
-  https://raw.githubusercontent.com/AdroitAdorKhan/antipopads-re/master/formats/domains.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/ultimate.txt
-  https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Hosts/GoodbyeAds.txt
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/wildcard.txt
-  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-  https://raw.githubusercontent.com/sjhgvr/oisd/main/domainswild_big.txt
-  https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/NoFormatting/cpbl-wildcard-blacklist.txt
 )
 
 # loop through the urls and download each file with curl
