a282779b61ad578fca0ec4febb557b9b14f8da7a
adv247 <40852159+adv247@users.noreply.github.com>
2023-08-20 12:30:53 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -14,6 +14,8 @@ urls=(
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/multi.txt
   
     )
 
