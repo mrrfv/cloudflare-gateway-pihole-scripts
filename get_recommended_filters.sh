9c41fa0eddcc785f9844d83760959006ad866eba
adv247 <40852159+adv247@users.noreply.github.com>
2023-08-21 12:11:50 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -46,7 +46,6 @@ urls=(
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/native.huawei.txt
   https://cdn.jsdelivr.net/gh/bogachenko/fuckfuckadblock@master/fuckfuckadblock.txt
   
-  
     )
 
 # loop through the urls and download each file with curl
