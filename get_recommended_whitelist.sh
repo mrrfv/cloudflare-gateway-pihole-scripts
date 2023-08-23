49c543b86e9683bb0ebd9d5fe93552f83e9c3aed
UlalaAZ <90678396+UlalaAZ@users.noreply.github.com>
2023-08-24 01:04:18 +0700

    Update get_recommended_whitelist.sh

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -30,6 +30,7 @@ urls=(
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt
     https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
+    https://raw.githubusercontent.com/nextdns/click-tracking-domains/main/domains
     # Commented out because it whitelists sites including doubleclick.net and ad.atdmt.com
     # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
 
