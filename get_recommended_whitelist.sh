590e7eb04c4c0c14a5b601d8c55a1e0c342b7885
Cuong Tien Dinh <me@ctdinh.io.vn>
2023-09-06 19:59:18 +0700

    Update get_recommended_whitelist.sh

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -32,7 +32,7 @@ urls=(
     # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
     # Uncomment the line below to use OISD's most commmonly whitelisted list
     # https://local.oisd.nl/extract/commonly_whitelisted.php
-
+    https://raw.githubusercontent.com/nextdns/click-tracking-domains/main/domains
 )
 
 # download all files in parallel and append them to whitelist.csv
