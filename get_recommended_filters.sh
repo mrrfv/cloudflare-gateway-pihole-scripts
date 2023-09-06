01aeb0d2fc100e9222f432e11bb157fa0e9f2b6c
Cuong Tien Dinh <me@ctdinh.io.vn>
2023-09-06 19:58:45 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -11,6 +11,9 @@ urls=(
   https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
   https://adaway.org/hosts.txt
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+  https://abpvn.com/filter/abpvn-cMipBM.txt
 )
 
 # download all files in parallel and append them to input.csv
