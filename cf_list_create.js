38d2f480244130ee8cf1255254821f5a4a4d687d
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2023-09-07 21:05:42 +0200

    Show how much over the domain limit we are

--- cf_list_create.js
+++ cf_list_create.js
@@ -101,8 +101,8 @@ fs.readFile('input.csv', 'utf8', async (err, data) => {
 
   // Trim array to 300,000 domains if it's longer than that
   if (domains.length > LIST_ITEM_LIMIT) {
+    console.warn(`${domains.length} domains found in input.csv - input has to be trimmed to ${LIST_ITEM_LIMIT} domains`);
     domains = trimArray(domains, LIST_ITEM_LIMIT);
-    console.warn(`More than ${LIST_ITEM_LIMIT} domains found in input.csv - input has to be trimmed`);
   }
 
   const listsToCreate = Math.ceil(domains.length / 1000);
