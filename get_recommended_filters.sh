dd8b741bf21a82d11743e05fd18859f178aafeca
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2023-09-07 21:09:23 +0200

    Update filter lists
    AntiMalwareHosts has to be removed because it makes the filter lists exceed the 300,000 domains limit. Furthermore, Cloudflare Zero Trust itself can be configured to block malware domains so this feels redundant.

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -8,7 +8,6 @@ urls=(
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
   https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
-  https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
   https://adaway.org/hosts.txt
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 )
