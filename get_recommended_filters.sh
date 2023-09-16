bb3e25b66fce8217960c328ab37b5c0bbc5e50a3
Viet Nam <74956696+bui-lang@users.noreply.github.com>
2023-09-17 01:18:25 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -11,7 +11,6 @@ urls=(
   https://raw.githubusercontent.com/Yhonay/antipopads/master/hosts
   https://malware-filter.gitlab.io/malware-filter/vn-badsite-filter-hosts.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/doh-vpn-proxy-bypass.txt
-  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/dyndns.txt
 )
 
 # download all files in parallel and append them to input.csv
