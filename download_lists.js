abd7d1c852b3440669473a43c0f9c7816e933259
Viet Huynh <xa.lach@yahoo.com>
2023-09-18 02:40:58 +0700

    removed pretty much every piece of shell script

--- download_lists.js
+++ download_lists.js
@@ -2,4 +2,57 @@ import { resolve } from "path";
 
 import { downloadFiles } from "./lib/utils.js";
 
-downloadFiles(resolve(process.argv[2]), process.argv.slice(3));
+const allowlistUrls = [
+  "https://raw.githubusercontent.com/im-sm/Pi-hole-Torrent-Blocklist/main/all-torrent-trackres.txt",
+  "https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt",
+  "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/whitelist.txt",
+  "https://raw.githubusercontent.com/TogoFire-Home/AD-Settings/main/Filters/whitelist.txt",
+  "https://raw.githubusercontent.com/freekers/whitelist/master/domains/whitelist.txt",
+  "https://raw.githubusercontent.com/DandelionSprout/AdGuard-Home-Whitelist/master/whitelist.txt",
+  "https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt",
+  "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt",
+  "https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/issues.txt",
+  "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/whitelist-referral.txt",
+  "https://raw.githubusercontent.com/mawenjian/china-cdn-domain-whitelist/master/china-cdn-domain-whitelist.txt",
+  "https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/hostnames.whitelist.txt",
+  "https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/mac.txt",
+  "https://raw.githubusercontent.com/boutetnico/url-shorteners/master/list.txt",
+  "https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/windows.txt",
+  "https://raw.githubusercontent.com/Dogino/Discord-Phishing-URLs/main/official-domains.txt",
+  "https://raw.githubusercontent.com/ookangzheng/blahdns/master/hosts/whitelist.txt",
+  "https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/android.txt",
+  "https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt",
+  "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt",
+  "https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt",
+  // Commented out because it whitelists sites including doubleclick.net and ad.atdmt.com
+  // https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt,
+  // Uncomment the line below to use OISD's most commmonly whitelisted list
+  // https://local.oisd.nl/extract/commonly_whitelisted.php,
+];
+const blocklistUrls = [
+  "https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt",
+  "https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt",
+  "https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt",
+  "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts",
+  "https://adaway.org/hosts.txt",
+  "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts",
+];
+const listType = process.argv[2];
+
+switch (listType) {
+  case "allowlist": {
+    await downloadFiles(resolve("./allowlist.txt"), allowlistUrls);
+    console.log(
+      "Done. The allowlist.txt file contains merged data from recommended whitelists."
+    );
+    break;
+  }
+  case "blocklist": {
+    await downloadFiles(resolve("./blocklist.txt"), blocklistUrls);
+    console.log(
+      "Done. The blocklist.txt file contains merged data from recommended filter lists."
+    );
+    break;
+  }
+  default:
+}
--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-# declare an array of urls
-urls=(
-  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
-  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
-  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
-  https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
-  https://adaway.org/hosts.txt
-  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-)
-
-# download all files in parallel and append them to blocklist.txt
-node download_lists.js blocklist.txt ${urls[@]}
-
-# print a message when done
-echo "Done. The blocklist.txt file contains merged data from recommended filter lists."
+node download_lists.js blocklist
