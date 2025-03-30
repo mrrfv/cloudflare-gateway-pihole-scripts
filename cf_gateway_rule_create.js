c5c1d7b4a199c008082dfcbf5fe1b33329f9da83
Ben Sykes <ben@bensykes.com>
2025-03-30 15:49:28 -0700

    Add script 'cloudflare-defragment' Which consolidates domains into fewer lists, deleting any empty lists. Domains are sorted by the time they were first added to one of our lists. This is done so that more stable entries end up in the earlier lists and domains that come and go are in the later lists. This enables the patching to be more efficient, touching only the later lists.

--- cf_gateway_rule_create.js
+++ cf_gateway_rule_create.js
@@ -1,32 +1,16 @@
-import { getZeroTrustLists, upsertZeroTrustRule } from "./lib/api.js";
+import { getZeroTrustLists, upsertZeroTrustDNSRule, upsertZeroTrustSNIRule } from "./lib/api.js";
 import { BLOCK_BASED_ON_SNI } from "./lib/constants.js";
 import { notifyWebhook } from "./lib/utils.js";
 
 const { result: lists } = await getZeroTrustLists();
 
-// Create a Wirefilter expression to match DNS queries against all the lists
-const wirefilterDNSExpression = lists.reduce((previous, current) => {
-  if (!current.name.startsWith("CGPS List")) return previous;
-
-  return `${previous} any(dns.domains[*] in \$${current.id}) or `;
-}, "");
-
-console.log("Checking DNS rule...");
-// .slice removes the trailing ' or '
-await upsertZeroTrustRule(wirefilterDNSExpression.slice(0, -4), "CGPS Filter Lists", ["dns"]);
+// Upsert DNS rules for all lists
+await upsertZeroTrustDNSRule(lists, "CGPS Filter Lists");
 
 // Optionally create a rule that matches the SNI.
 // This only works for users who proxy their traffic through Cloudflare.
 if (BLOCK_BASED_ON_SNI) {
-  const wirefilterSNIExpression = lists.reduce((previous, current) => {
-    if (!current.name.startsWith("CGPS List")) return previous;
-
-    return `${previous} any(net.sni.domains[*] in \$${current.id}) or `;
-  }, "");
-
-  console.log("Creating SNI rule...");
-  // .slice removes the trailing ' or '
-  await upsertZeroTrustRule(wirefilterSNIExpression.slice(0, -4), "CGPS Filter Lists - SNI Based Filtering", ["l4"]);
+  await upsertZeroTrustSNIRule(lists, "CGPS Filter Lists - SNI Based Filtering");
 }
 
 // Send a notification to the webhook
